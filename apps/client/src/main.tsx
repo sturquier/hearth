59d8c562aa33007773c0cc251f13c2409b6f29fb
sturquier <turquiersimon@hotmail.fr>
2025-12-17 14:06:36 +0100

    Setup client tests config

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -8,6 +8,12 @@ import {
   getContext,
 } from './providers/TanstackQueryProvider';
 
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 const rootElement = document.getElementById('app');
 
 if (rootElement && !rootElement.innerHTML) {
